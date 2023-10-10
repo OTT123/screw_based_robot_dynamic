c12c662e9582da34fd7f2700b0e91175d82c9036
Zhilin_Xu <99379651+OTT123@users.noreply.github.com>
2023-10-10 10:46:22 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "./my_dynamic.h"
-#include "symrobotic.h"
 #include <windows.h>
 
 
@@ -8,23 +7,23 @@ int main()
     //测试
     my_Robot_dynamic robot;
     //ur5机械臂 
-    std::string link_name[DOF + 1] = { "base_link" , "shoulder_link" ,"upper_arm_link" ,"forearm_link" ,"wrist_1_link" ,"wrist_2_link" ,"wrist_3_link" };
-    std::string axis_name[DOF] = { "shoulder_pan_joint","shoulder_lift_joint","elbow_joint","wrist_1_joint","wrist_2_joint","wrist_3_joint" };
-    std::string urdf_name = "D:\\Desktop\\CPP_pj\\new_dyn_2\\new_dyn_2\\urdf\\ur5.urdf";
+    //std::string link_name[DOF + 1] = { "base_link" , "shoulder_link" ,"upper_arm_link" ,"forearm_link" ,"wrist_1_link" ,"wrist_2_link" ,"wrist_3_link" };
+    //std::string axis_name[DOF] = { "shoulder_pan_joint","shoulder_lift_joint","elbow_joint","wrist_1_joint","wrist_2_joint","wrist_3_joint" };
+    //std::string urdf_name = ".\\urdf\\ur5.urdf";
     //gluon机械臂 
-    //std::string link_name[DOF + 1] = { "base_link" , "1_Link" ,"2_Link", "3_Link" ,"4_Link" ,"5_Link" ,"6_Link" };
-    //std::string axis_name[DOF] = { "axis_joint_1","axis_joint_2","axis_joint_3","axis_joint_4","axis_joint_5","axis_joint_6" };
-    //std::string urdf_name = "D:\\Desktop\\CPP_pj\\new_dyn_2\\new_dyn_2\\urdf\\gluon.urdf";
+    std::string link_name[DOF + 1] = { "base_link" , "1_Link" ,"2_Link", "3_Link" ,"4_Link" ,"5_Link" ,"6_Link" };
+    std::string axis_name[DOF] = { "axis_joint_1","axis_joint_2","axis_joint_3","axis_joint_4","axis_joint_5","axis_joint_6" };
+    std::string urdf_name = ".\\urdf\\gluon.urdf";
 
     //puma560机械臂 
     //std::string link_name[DOF + 1] = { "base_link" , "Link1" ,"Link2" ,"Link3" ,"Link4" ,"Link5" ,"Link6" };
     //std::string axis_name[DOF] = { "J1","J2","J3","J4","J5","J6" };
-    //std::string urdf_name = "D:\\Desktop\\CPP_pj\\new_dyn_2\\new_dyn_2\\urdf\\puma560_robot.urdf";
+    //std::string urdf_name = ".\\urdf\\puma560_robot.urdf";
 
     //sw导出的珞石机械臂urdf
     //std::string link_name[DOF + 1] = { "base_link" , "1_Link" ,"2_Link" ,"3_Link" ,"4_Link" ,"5_Link" ,"6_Link" };
     //std::string axis_name[DOF] = { "1_joint","2_joint","3_joint","4_joint","5_joint","6_joint" };
-    //std::string urdf_name = "D:\\Desktop\\CPP_pj\\new_dyn_2\\new_dyn_2\\urdf\\test.SLDASM.urdf";
+    //std::string urdf_name = ".\\urdf\\test.SLDASM.urdf";
 
     std::cout << "parsing robot urdf @: " + urdf_name << std::endl;
     robot.urdf_parser(urdf_name, link_name, axis_name);
@@ -46,7 +45,7 @@ int main()
     Eigen::Matrix<double, DOF, 1> corMat;
     Eigen::Matrix<double, DOF, 1> graMat;
 
-    int count = pow(2,11);   //计算count次用于测量时间性能
+    int count = pow(2,10);   //计算count次用于测量时间性能
     LARGE_INTEGER t1, t2, tc;
     std::cout << "***********************" << std::endl;
     QueryPerformanceFrequency(&tc);
